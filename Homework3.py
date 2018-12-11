756dc80acc79290b198ef376579224b965e4b9a4
Inna Dolina <idol88@ukr.net>
2018-12-11 23:45:54 +0200

    task 3 done

--- Homework3.py
+++ Homework3.py
@@ -22,10 +22,25 @@ def some_tuple(array):
         if array.index(x) % 2 == 0:
             var_1 = var_1 + x
     print("Sum of even index elements is: ", var_1)
-    var_2 = var_1*len(array)
+    var_2 = var_1 * len(array)
     print("Multiplied previous result with the last element: ", var_2)
 
 
 some_tuple((1, 5, 38, 0, 33, 6))
 
 
+# task 3
+def replace_func(text):
+    for ch in ["*", "_", "{", "}", "[", "]", "(", ")", ">",
+               "<", "=", "#", "+", "-", ".", "!", "?", "$",
+               "&", "%", "^", ":", ";", "@"]:
+        text = text.replace(ch, " ")
+    return text
+
+
+def the_first_word_in_string(your_str):
+    splited = replace_func(your_str).split()
+    print(splited[0])
+
+
+the_first_word_in_string("      ...!?@jffkfkfk jg !!! jhb * gjGGHDDD=jbjb")
