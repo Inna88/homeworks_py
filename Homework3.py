8781ca5eaf83f6fa099f9f455b6c9924dbc75ea8
Inna Dolina <idol88@ukr.net>
2018-12-11 21:21:53 +0200

    task 2 done

--- Homework3.py
+++ Homework3.py
@@ -11,3 +11,21 @@ def all_capitals(text):
 
 
 all_capitals("How are you? Eh, ok. Low or Lower? Ohhh.")
+
+
+# task 2
+
+
+def some_tuple(array):
+    var_1 = 0
+    for x in array:
+        if array.index(x) % 2 == 0:
+            var_1 = var_1 + x
+    print("Sum of even index elements is: ", var_1)
+    var_2 = var_1*len(array)
+    print("Multiplied previous result with the last element: ", var_2)
+
+
+some_tuple((1, 5, 38, 0, 33, 6))
+
+
